a09c4c754f8be82ef95d49126bf16b0fe8b6124a
Russell Macalintal <russell.macalintal@gmail.com>
2020-03-27 03:32:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -13,10 +13,20 @@ class PigLatinizer
   end
 
   def pig_latin(word)
-    if word.chars.first =~ /[aeiou]/
+    if word.chars[0] =~ /[aeiou]/
       word + "way"
     else
-      if word.
+      if word.chars[1] =~ /[aeiou]/
+        word.chars.rotate.join + "ay"
+      else
+        if word.chars[2] =~ /[aeiou]/
+          word.chars.rotate(2).join + "ay"
+        else
+          if word.chars[3] =~ /[aeiou]/
+            word.chars.rotate(3).join + "ay"
+          end
+        end
+      end
     end
   end
 
