e661c6a5b766fb536db9f56f2d435b1b54a18c02
Russell Macalintal <russell.macalintal@gmail.com>
2020-03-27 04:26:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @orig_phrase = params[:user_phrase]
+    @translator = PigLatinizer.new
+    erb :translated
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,26 @@
+class PigLatinizer
+
+  def piglatinize(text)
+    text_arr = text.split(" ")
+    text_arr.collect { |word| self.convert(word) }.join(" ")
+  end
+
+  def convert(word)
+    if word.downcase.chars[0] =~ /[aeiou]/
+      word + "way"
+    else
+      if word.chars[1] =~ /[aeiou]/
+        word.chars.rotate.join + "ay"
+      else
+        if word.chars[2] =~ /[aeiou]/
+          word.chars.rotate(2).join + "ay"
+        else
+          if word.chars[3] =~ /[aeiou]/
+            word.chars.rotate(3).join + "ay"
+          end
+        end
+      end
+    end
+  end
+
+end
